8bddfcb34bee40e31a4f713bb2e0d9bc5aeaadae
Ruben Kalmbach <85735123+RubKa@users.noreply.github.com>
2023-03-28 10:38:01 +0200

    init

---
+++ Prototyp/Nodes/Activity.cs
@@ -0,0 +1,15 @@
+﻿using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
+
+namespace Prototyp.Nodes
+{
+    public class Activity : NodeModel
+    {
+        public Activity(Blazor.Diagrams.Core.Geometry.Point position = null) : base(position)
+        {
+
+        }
+
+        public string Description { get; set; }
+    }
+}
---
+++ Prototyp/Nodes/Gateway.cs
@@ -0,0 +1,17 @@
+﻿using Blazor.Diagrams.Core.Models;
+
+namespace Prototyp.Nodes
+{
+    public class Gateway : NodeModel
+    {
+        public Gateway(Blazor.Diagrams.Core.Geometry.Point? point = null) : base(point)
+        {
+            this.AddPort(PortAlignment.BottomLeft);
+            this.AddPort(PortAlignment.BottomRight);
+            this.AddPort(PortAlignment.TopLeft);
+            this.AddPort(PortAlignment.TopRight);
+        }
+
+        public string Description { get; set; }
+    }
+}
